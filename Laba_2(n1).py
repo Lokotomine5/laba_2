1366bf25858a2f742e99370dd1d3834f813cdb28
Lokotomine5 <114746101+Lokotomine5@users.noreply.github.com>
2026-09-08 19:06:16 +0300

    33

--- Laba_2(n1).py
+++ Laba_2(n1).py
@@ -1,16 +1,1 @@
-import math
-x = float(input("Введите x (x < -1): "))
-n = int(input("Введите n: "))
 
-s = -math.pi/2
-xrazr = x
-steps = 0
-for n in range(1000):
-    v = (-1) ** (n+1) / ((2 * n + 1) * xstep)
-    s += v
-    steps += 1
-    xrazr *= x * x
-    if abs(v) < n:
-        break
-print('сумма ряда: ', s)
-print('кол-во шагов: ', steps)
