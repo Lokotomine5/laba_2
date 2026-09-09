ca220f2ea2024672d51c5e90dc24c0ac0f1a70d4
Lokotomine5 <114746101+Lokotomine5@users.noreply.github.com>
2026-09-09 22:27:07 +0300

    Update laba_2(n1(2.0)).py

--- laba_2(n1(2.0)).py
+++ laba_2(n1(2.0)).py
@@ -5,18 +5,17 @@ n = int(input("Введите n: "))
 s = -math.pi/2
 xrazr = x
 steps = 0
-for n in range(1000):
-    v = (-1) ** (n+1) / ((2 * n + 1) * xrazr)
+for b in range(n):
+    v = (-1) ** (b+1) / ((2 * b + 1) * xrazr)
     s += v
     steps += 1
     xrazr *= x * x
-    if abs(v) < n:
-        break
+
 print('сумма ряда: ', s)
 print('кол-во шагов: ', steps)
 
 d= -math.pi/2
 for q in range(n):
     h = (-1) ** (q+1)/((2*q+1)*x**(2*q+1))
-    d += v
+    d += h
 print ('сумма ряда не опт вариант =', d)
